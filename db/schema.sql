fd14dc0cc0e1981d0a5132099758ef3f90cce8b4
SummerMedusa <50718811+SummerDarcel@users.noreply.github.com>
2019-09-18 17:34:54 -0700

    db schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,9 @@
-CREATE burgers_db;
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+CREATE TABLE burgers (
+    burger_id INT (10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR (50) NOT NULL,
+    devoured BOOLEAN DEFALT false NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+
+VALUES
+("Royal with Cheese","Big Kahuna","Krusty Burger","Wolf Burger-Hokkaido","B,de Burger-Rio","Burgerlich-Hamburg-er", "Uniburger-Montreal","Double Patties-Perth","Bermeister with Bacon-Berlin"
+);
