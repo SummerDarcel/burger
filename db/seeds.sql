b674e8172e6baab0840312d4cc80706e748d84ea
SummerMedusa <50718811+SummerDarcel@users.noreply.github.com>
2019-09-19 17:50:10 -0700

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,38 @@
 USE burgers_db;
 
 INSERT INTO burgers (burger_name)
-
 VALUES
-("Royal with Cheese","Big Kahuna","Krusty Burger","Wolf Burger-Hokkaido","B,de Burger-Rio","Burgerlich-Hamburg-er", "Uniburger-Montreal","Double Patties-Perth","Bermeister with Bacon-Berlin"
+("Royal with Cheese");
+
+INSERT INTO burgers (burger_name)
+VALUES
+("Big Kahuna");
+
+INSERT INTO burgers (burger_name)
+VALUES
+("Krusty Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES
+("Wolf Burger-Hokkaido");
+
+INSERT INTO burgers (burger_name)
+VALUES
+("B,de Burger-Rio");
+
+INSERT INTO burgers (burger_name)
+VALUES
+("Burgerlich-Hamburg-er");
+
+INSERT INTO burgers (burger_name)
+VALUES
+("Uniburger-Montreal");
+
+INSERT INTO burgers (burger_name)
+VALUES
+("Double Patties-Perth");
+
+INSERT INTO burgers (burger_name)
+VALUES
+("Bermeister with Bacon-Berlin"
 );
