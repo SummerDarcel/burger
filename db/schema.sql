83bdc1a03c7281e70de00cb18415833a8b886239
SummerMedusa <50718811+SummerDarcel@users.noreply.github.com>
2019-09-22 12:04:44 -0700

    changed sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 CREATE TABLE burgers (
-    burger_id INT (10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT (10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     burger_name VARCHAR (50) NOT NULL,
     devoured BOOLEAN DEFAULT false
 );
